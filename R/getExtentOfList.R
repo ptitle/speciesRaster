217295493073463bc3104c7d03e4222b8945eaaa
ptitle <pascaltitle@gmail.com>
2019-03-14 17:14:00 -0400

    add sf compatibility to getExtentOfList

--- R/getExtentOfList.R
+++ R/getExtentOfList.R
@@ -3,7 +3,7 @@
 ##' @description Given a list of SpatialPolygons, return an extent
 ##' object that encompasses all items. 
 ##'
-##' @param shapes a list of SpatialPolygons
+##' @param shapes a list of SpatialPolygons or simple features
 ##'
 ##' @return An object of class \code{extent}. 
 ##'
@@ -15,12 +15,21 @@
 ##' @export
 
 
-getExtentOfList <- function(shapes) {	
-	x <- lapply(shapes, function(x) sp::bbox(x))
-	minLong <- min(sapply(x, function(x) x[1], simplify = TRUE))
-	maxLong <- max(sapply(x, function(x) x[3], simplify = TRUE))
-	minLat <- min(sapply(x, function(x) x[2], simplify = TRUE))
-	maxLat <- max(sapply(x, function(x) x[4], simplify = TRUE))
+getExtentOfList <- function(shapes) {
+	
+	if (class(shapes[[1]]) %in% c('SpatialPolygons', 'SpatialPolygonsDataFrame')) {	
+		x <- lapply(shapes, function(x) sp::bbox(x))
+		x <- lapply(x, function(y) c(xmin = y[1,1], ymin = y[2,1], xmax = y[1,2], ymax = y[2,2]))
+	} else if ('sf' %in% class(shapes[[1]])) {
+		x <- lapply(shapes, function(x) sf::st_bbox(x))
+	} else {
+		stop('shapes object not recognized.')
+	}
+
+	minLong <- min(sapply(x, function(x) x$xmin, simplify = TRUE))
+	maxLong <- max(sapply(x, function(x) x$xmax, simplify = TRUE))
+	minLat <- min(sapply(x, function(x) x$ymin, simplify = TRUE))
+	maxLat <- max(sapply(x, function(x) x$ymax, simplify = TRUE))
 	
 	res <- raster::extent(shapes[[1]])
 	res@xmin <- minLong
